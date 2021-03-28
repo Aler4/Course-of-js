6185e656fa4927f73a1a19bcb73e8080d6a0cacd
unknown <alerch5666@gmail.com>
2021-03-29 01:14:22 +0300

    homework heirdom

--- main.js
+++ main.js
@@ -1,152 +1,121 @@
+// 1 . Cоздать базовый класс User и отнаследовать
+// от него Admin в двух стилях - функциональном и на классах
 
- // 1. Дан массив с числами. С помощью цикла выведите только те элементы массива, которые больше нуля и меньше 10-ти;
+// ***Функиональное
 
-// arr1 = [-34, 0,  8, 7, 9, 11, 22, -2, 553];
 
-// ***Method #1
 
-// for( value of arr1) {
-//   if (value > 0 && value< 10) {
-//     console.log(value)
+//  function User(name, age, hobbys) {
+
+//   this.name = name;
+//   this.age = age;
+//   this.hobbys = [hobbys];
+
+//    this.breakSomething = function() {
+//     console.error( ` Hi I user: ${this.name}, I will break something here! :(` )
 //   }
 // };
 
-// ***Method #2
+// function Admin(...args){
+//     User.call(this, ...args)
+//     this.fixSomething = function() {
+//     console.warn(`Hi! I admin: ${this.name}, I will fix
+//     everthing but anyway we will find you ${user1.name}`)
+//   }
+// };
 
-// function getSpecArr(arr) {
-//   let lessValue = 0;
-//   let bigValue = 0;
-//   arr = arr.sort((a, b) => {
-//     return a -b;
-//   })
 
-//   for( i = 1; i <= arr.length - 1 ; i++) {
-//       if (arr[i] > 0 && arr[i] < 10) {
-//         console.log(arr[i])
-//       }
-//       else if (arr[i] < 0) {
-//         lessValue = i + 1;
-//         continue;
+// const user1 = new User('Neo', 25, 'be cool');
+// const admin1 = new Admin('Smith', Infinity, 'destruction of humanity');
+
+// user1.breakSomething();
+// admin1.fixSomething();
+// console.log(user1);
+// console.log(admin1);
+
+
+
+// *****ООП
+
+// class User {
+
+//   constructor(name, age, hobbys) { 
+//     this.name = name;
+//     this.age = age;
+//     this.hobbys = [hobbys];
+//         this.breakSomething = function() {
+//     console.error( ` Hi I user: ${this.name}, I will break all here! :)` )
+//   }
+// }
+// }
+
+// class Admin extends User {
+//   constructor(...args) {
+//     super(...args)
+//     this.fixSomething = function (){
+//     console.warn(`Hi! I admin: ${this.name}, please do not :(`);
 //     }
-//     else if (arr[i] > 10) {
-//       bigValue = arr.length - i;
-//       break;
-//     }
+
 //   }
-//       console.log(`Значений меньше 0: ${lessValue}`)
-//       console.log(`Значений больше 10: ${bigValue}`)
+// }
 
-// };
-//  getSpecArr(arr1);
+// const user1 = new User('Masha', 8, 'irritate');
+// const admin1 = new Admin('Bear', 45, 'quiet life');
 
-// 2. Дан массив с числами. Найдите среднее арифметическое его элементов;
+// user1.breakSomething();
+// admin1.fixSomething();
 
-// let arr2 = [1, 2, 3, 8, 13, 46];
-// function getMean(arr) {
-//   let total = 0;
-//   let sum = arr.length;
-//   for( num of arr){
-//     total +=num;
-//   }
-//   console.log(Math.round(total/sum))
-// };
-// getMean(arr2);
+function Machine(power) {
+  this._power = power;
+  this._enabled = false;
 
-// 3. Дан массив с числами. С помощью цикла проверьте, что в нем есть элемент со значением 5.
-// Как только будет найден первый такой элемент - выведите 'Есть' и оборвите цикл.
-// Если такого элемента нет - ничего не выводите. 
+  var self = this;
 
-// let arr3 = [3,4, 6, 8, 5, 9,];
+  this.enable = function() {
+    self._enabled = true;
+  };
 
-// function findValue(arr, value) {
-//   for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] == value) {
-//       console.log('Ecть');
-//       console.log('Итераций: '+ i);
-//       break;
-//     }
-//   }
-//    return console.log('Значения нет');
-// };
-// findValue(arr3,5);
+  this.disable = function() {
+    self._enabled = false;
+  };
+}
 
-// 4) Создайте объект cat, name:'Stepan' и в этом объекте создайте метод voice,
-//  который при вызове себя у объекта cat будет выводить сообщение в консоль 'привет, меня зовут Stepan';
+function Fridge() {
 
-// let cat = {
-//   name: 'Stepan',
-//    voice(){
-//     console.log(`Привет, меня зовут ${this.name}, мяу или типо того`)
-//   }
-// };
+  Machine.call(this, +arguments[0]);
 
-// cat.voice();
+  let volume = +arguments[0] / 100;
 
-// 5. Программное обеспечение для распознавания символов широко используется для оцифровки печатных текстов.
-// Таким образом, тексты можно редактировать, искать и сохранять на компьютере.
-// Когда документы (особенно старые, написанные на пишущей машинке) оцифровываются,
-// программы распознавания символов часто делают ошибки. Ваша задача - исправить ошибки в оцифрованном тексте
-// . Вам нужно только исправить следующие случаи: S-5, O-0, I-1,
-//  correct('L0ND0N') => 'LONDON'
-// correct('DUBL1N') => 'DUBLIN'
-// correct('51NGAP0RE') => 'SINGAPORE'
-// correct('BUDAPE5T') => 'BUDAPEST'
-// correct('PAR15') => 'PARIS'
+  this.food = [];
 
+  this.addFood = function(...foods) {
 
-// ***Method #1
+    (this._enabled == false) ?
+    console.error('ошибка, холодильник выключен') :
+    this.food.push(...foods);
 
-// function correct(str) {
-//    corStr =str.split('');
+    if (this.food.length > volume){
 
-//   for(i = 0; i <= corStr.length; i++) {
-//     switch(corStr[i]) {
-//       case '5' ||  5:
-//           corStr.splice(i, 1, 'S')
-//         continue;
-//       case '0' || 0:
-//           corStr.splice(i, 1, 'O');
-//         continue;
-//       case '1' || 1:
-//           corStr.splice(i, 1, 'I');
-//         continue;
-//     }
-//   } 
+      this.food = this.food.slice(0,volume)
+     console.error(' ошибка, слишком много еды')
 
-//   return console.log(`${str} => ${corStr.join('')}`); 
-// };
+    };
+  }
 
-// ***Method #2
+  this.getFood = function() { return this.food; } // Увы, как сделать массив неизменяемым я не допер :(
+}
 
-function correct(str) {
-   corStr  = str.split('');
+let fridge = new Fridge(500);
+fridge.enable();
 
-  for(letter in corStr) {
-    switch(corStr[letter]) {
-      case '5' ||  5:
-          corStr[letter] = 'S';
-        continue;
-      case '0' || 0:
-         corStr[letter] = 'O';
-        continue;
-      case '1' || 1:
-          corStr[letter] = 'I';
-        continue;
-    }
-  } 
+fridge.addFood("котлета");
+fridge.addFood("сок", "варенье");
 
-  return console.log(`${str} => ${corStr.join('')}`); 
-};
-correct('L0ND0N');
-correct('DUBL1N');
-correct('51NGAP0RE');
-correct('BUDAPE5T');
-correct('PAR15');
+var fridgeFood = fridge.getFood();
+alert( fridgeFood )
 
+fridgeFood.push("вилка", "ложка");
 
-
-
-
-
+alert( fridge.getFood() )
 
 
