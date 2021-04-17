3aa0172851bebe128d64c65cffcc32e33406e9f3
unknown <alerch5666@gmail.com>
2021-04-17 15:24:11 +0300

    'cart_with_callback/promise'

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const section = document.querySelector('.product');
 let products;
 let inCartCount = 0;
 let inCartSum = 0;
-let totalCount = document.querySelector('.count');
+let totalCount = document.querySelectorAll('.count');
 let totalSum = document.querySelector('.summary');
-let id = Date.now()
-
 let btnCart = document.querySelector('.btn-cart');
 let cart = document.querySelector('.cart');
+let cartArr = [];
+let sortList = document.querySelector('.sort-list');
+
+
+let coffee_items;
 
 let xhr = new XMLHttpRequest();
 xhr.open('GET', url, true); // конфигурируем запрос
@@ -24,10 +27,14 @@ xhr.onload = () => {
 }
 
 
+
+
 function buildLayout(data){
   data.forEach(item => {
     let div = document.createElement('div');
     div.classList.add('coffee_item');
+    div.setAttribute('data-price', item.price);
+    div.setAttribute('data-name', item.name);
 
     let img = new Image();
     img.src= item.img;
@@ -48,7 +55,7 @@ function buildLayout(data){
 
     section.append(div);
   });
-
+  coffee_items = document.querySelectorAll('.coffee_item');
 }
 
 section.addEventListener('click', function(event){
@@ -59,45 +66,134 @@ section.addEventListener('click', function(event){
 
 
 function addToCart(el){
+  let id = Date.now();
+  let bodge =  document.querySelector('.bodge');
   let cartList = document.querySelector('.cart-list');
   let cartListItem = document.createElement('li');
-  let priceSpan = document.createElement('span');
-  let name = el.querySelector('h3').innerText.trim();
-  let price = +el.querySelector('h2').innerText.trim();
+  let cartListItemContent = document.createElement('p');
+  cartListItemContent.classList.add('list-content')
+  let cartListPrice = document.createElement("i");
 
+
+  cartListItem.id = id;
+  let name = el.querySelector('h3').innerText.trim();
+  let price = +el.querySelector('h2').innerHTML;
   let minusBtn = document.createElement('span');
-  minusBtn.innerText= '-';
+  minusBtn.innerHTML= '<i class="fas fa-minus-circle"></i>';
   minusBtn.classList.add('minus-item');
 
   inCartSum += price;
   inCartCount++;
-  totalCount.innerText = inCartCount;
+
+  totalCount.forEach((item) => {
+    item.innerText = inCartCount
+  })
   totalSum.innerText = inCartSum;
-
-  cartListItem.append(`${name}:  ${price}ua`,minusBtn);
+  cartListPrice.append(`${price}`)
+  cartListItemContent.append(`${name}: `, cartListPrice, minusBtn)
+  cartListItem.append(cartListItemContent,);
   cartList.append(cartListItem,);
+  cartArr.push([name,price]);
 
+  showBodge(inCartCount, bodge);
 
+    // ******Callback
 
+  // minusBtn.addEventListener('click', (e) => {msgDel(minusBtn.parentNode.innerText,del,notDel)})
+
+  // function msgDel(e,yes,no){
+  //   let result = confirm(`Do you want to remove ${e}`);
+  //   result ? yes() : no()
+  // };
+  //
+  // function del() {
+  //   // debugger
+  //   minusBtn.parentNode.remove();
+  //   totalCount.forEach((item) => {
+  //     item.innerText = +item.innerText - 1;
+  //     inCartCount = totalCount[0].innerText;
+  //   })
+  //   inCartSum = totalSum.innerText - cartListPrice.innerText;
+  //   totalSum.innerText = inCartSum;
+  //   cartArr.splice(inCartCount - 1, 1)
+  //   alert(`${minusBtn.parentNode.innerText} has been remove!`)
+  // };
+  //
+  // function notDel(){
+  //   alert(`${minusBtn.parentNode.innerText}not remove!`)
+  // };
+
+  // **** Promise****
+
+  minusBtn.addEventListener('click', (e) => {delPromise()})
+
+  function delPromise() {
+    return new Promise(function (resolve, reject) {
+      let cart = document.querySelector('.cart');
+      cart.style.display = 'none';
+      let msg = document.createElement('div');
+      msg.classList.add('msg');
+      msg.innerHTML = `<p>Do you want to delete ${minusBtn.parentNode.innerText}?</p>`;
+      let yesBtn = document.createElement('button');
+      yesBtn.classList.add('yes-btn');
+      yesBtn.innerText= 'Yes';
+      let noBtn = document.createElement('button');
+      noBtn.classList.add('no-btn');
+      noBtn.innerText= 'No';
+
+      msg.append(yesBtn, noBtn);
+      document.body.append(msg)
+      resolve()
+      reject()
+    })
+        .then(() => {
+          let agree = document.querySelector('.yes-btn')
+          let disagree = document.querySelector('.no-btn')
+          agree.addEventListener('click', () => {
+            minusBtn.parentNode.remove();
+            totalCount.forEach((item) => {
+              item.innerText = +item.innerText - 1;
+              inCartCount = totalCount[0].innerText;
+            })
+            inCartSum = totalSum.innerText - cartListPrice.innerText;
+            totalSum.innerText = inCartSum;
+            cartArr.splice(inCartCount - 1, 1)
+            alert(`${minusBtn.parentNode.innerText} was removed!`)
+            document.querySelector('.msg').remove()
+            cart.style.display = 'block';
+
+          })
+          disagree.addEventListener('click', () => {
+            alert(`${minusBtn.parentNode.innerText} was not removed!`)
+            document.querySelector('.msg').remove()
+            cart.style.display = 'block';
+
+          })
+        })
+  }
 }
 
 
-btnCart.addEventListener('click',(e) => {
-  cart.style.opacity = '1';
-  cart.style.visibility = 'visible';
-})
+function showBodge(count, element) {
+  if(count > 0) {
+    element.style.opacity = '1'
+  }
+  else
+    element.style.opacity = '0'
+}
+
 cart.addEventListener('click',(e) => {
   if (e.target == document.querySelector('.close-cart')){
     cart.style.opacity = '0';
     cart.style.visibility = 'hidden';
   }
-  if (e.target == document.querySelector('.minus-item')){
-
-    totalCount.innerText = +totalCount.textContent -1;
-    // totalSum.innerText = +totalSum.textContent - +(e.target.parenNode.price);
-    e.target.parentNode.remove();
-  }
 })
+
+btnCart.addEventListener('click',(e) => {
+  cart.style.opacity = '1';
+  cart.style.visibility = 'visible';
+})
+
 let icons = document.querySelector('.icon-container');
 
 icons.addEventListener("click", (e) => {
@@ -116,6 +212,36 @@ icons.addEventListener("click", (e) => {
     sortList.style.left = '-100%';
   }
 
-      })
+})
+
+sortList.addEventListener('click', (e) => {
+  if (e.target == document.querySelector('.name')) {
+    console.log(1)
+    sortProducts(coffee_items, 'name');
+  }
+  else if (e.target == document.querySelector('.price')) {
+    sortProducts(coffee_items, 'price');
+  }
+})
+
+function sortProducts(data, prop){
+  let dataCopy = [].slice.call(data);
+
+  dataCopy.sort(function(a,b){ // a,b - это обьекты из массива products, которые мы попарно сравниваем
+    if(a.getAttribute(`data-${prop}`) > b.getAttribute(`data-${prop}`)){ // если prop в обьекте а больше prop в обьекте b, то в массиве а перемещается после b
+      return 1;
+    }
+    else  if(a.getAttribute(`data-${prop}`) < b.getAttribute(`data-${prop}`)){ // если prop в обьекте b больше prop в обьекте a, то в массиве b перемещается после a
+      return -1;
+    }
+    else { // здесь ничего не меняем местами
+      return 0;
+    }
+  });
+
+  console.log(dataCopy);
+
+  dataCopy.forEach((item, index) => item.style.order = index);
+}
 
 
